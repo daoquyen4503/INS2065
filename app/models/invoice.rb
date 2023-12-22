1a0d1db5c10aba70877b8d486c9c5332127faf45
Quoc <irextwenty2@gmail.com>
2023-12-22 21:51:09 +0700

    Polishing plate boiler content

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :staff
 
-  validates_presence_of :customer, :title, :staff, :quantity, :total_price, date
+  validates_presence_of :customer, :title, :staff, :quantity, :total_price, :date
   validates_numericality_of :quantity, :total_price
 
   validate :date_cannot_be_in_the_future
